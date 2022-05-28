69a88c56f34d04d607e6982a6ef55ebdaf8f81d9
SlimeyApprentice <jamesknepper03@gmail.com>
2022-05-28 03:07:37 +0000

    update graphics and interface

--- core.py
+++ core.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 import os
+import tty
+import sys
+import termios
 
 
 WINDEX = {"O": ["+---+", "|   |", "|   |", "|   |", "+---+"],
@@ -97,6 +100,12 @@ def check_board(state):
 # print([check_board(k) for k in [test0, test1, test2, test3, test4, test5]])
 
 
+def color(string, b):
+    if b:
+        return "\033[5;7m"+string+"\033[0m"
+    return string
+
+
 class MiniBoard:
     def __init__(self):
         self.__state = [" "]*9
@@ -112,17 +121,17 @@ class MiniBoard:
             return 1, state
         return 0, " "
 
-    def render(self, line, is_host):
+    def render(self, line, selected, in_focus, move):
         cap1 = ""
         cap2 = ""
-        if is_host:
+        if in_focus:
             cap1 = "\033[5;7m"
             cap2 = "\033[0m"
         if self.__winner != " ":
-            return WINDEX[self.__winner][line]
+            return cap1+WINDEX[self.__winner][line]+cap2
         if line % 2 == 0:
             true_line = line // 2
-            return cap1+"│".join([self.__state[true_line*3 + k] for k in range(3)])+cap2
+            return cap1+"│".join([color(self.__state[true_line*3 + k], true_line*3+k == move and selected) for k in range(3)])+cap2
         return cap1+"─┼─┼─"+cap2
 
     def winner(self):
@@ -149,35 +158,52 @@ class Board:
             return 0, " "
         return 0, " "
 
-    def render(self, host):
+    def render(self, host, action):
         cap1 = ""
         cap2 = ""
-        if host == -1:
-            cap1 = "\033[5;7m"
-            cap2 = "\033[0m"
+        # if host == -1:
+        #     cap1 = "\033[5;7m"
+        #     cap2 = "\033[0m"
         print(cap1+"       │       │       "+cap2)
         for i in range(5):
-            print(cap1+" "+self.__state[0].render(i, host == 0)+" │ "+self.__state[1].render(i, host == 1)+" │ " +
-                  self.__state[2].render(i, host == 2)+" "+cap2)
+            print(cap1+" "+self.__state[0].render(i, host == 0, (host == -1 and action == 0), action)+" │ "+self.__state[1].render(i, host == 1, (host == -1 and action == 1), action)+" │ " + self.__state[2].render(i, host == 2, (host == -1 and action == 2), action)+" "+cap2)
         print(cap1+"       │       │       "+cap2)
         print(cap1+"───────┼───────┼───────"+cap2)
         print(cap1+"       │       │       "+cap2)
         for i in range(5):
-            print(cap1+" "+self.__state[3].render(i, host == 3)+" │ "+self.__state[4].render(i, host == 4)+" │ " +
-                  self.__state[5].render(i, host == 5)+" "+cap2)
+            print(cap1+" "+self.__state[3].render(i, host == 3, (host == -1 and action == 3), action)+" │ "+self.__state[4].render(i, host == 4, (host == -1 and action == 4), action)+" │ " + self.__state[5].render(i, host == 5, (host == -1 and action == 5), action)+" "+cap2)
         print(cap1+"       │       │       "+cap2)
         print(cap1+"───────┼───────┼───────"+cap2)
         print(cap1+"       │       │       "+cap2)
         for i in range(5):
-            print(cap1+" "+self.__state[6].render(i, host == 6)+" │ "+self.__state[7].render(i, host == 7)+" │ " +
-                  self.__state[8].render(i, host == 8)+" "+cap2)
+            print(cap1+" "+self.__state[6].render(i, host == 6, (host == -1 and action == 6), action)+" │ "+self.__state[7].render(i, host == 7, (host == -1 and action == 7), action)+" │ " + self.__state[8].render(i, host == 8, (host == -1 and action == 8), action)+" "+cap2)
         print(cap1+"       │       │       "+cap2)
 
     def check_open(self, grid):
         return self.__state[grid].is_open()
 
 
+def render(board, host, action, player, flag, winner, report):
+    os.system("clear")
+    board.render(host, action)
+    if flag == -1:
+        print("that's not a valid move:", report)
+    elif flag == 1:
+        print(winner, "WINS!")
+        return False
+    print(player, "to play")
+    if host >= 0:
+        print("host x:", (host % 3) + 1, "y: ", (host // 3) + 1)
+    else:
+        print("specify host\n\n>")
+    return True
+
+
+
+
 def main():
+    original_terminal_settings = termios.tcgetattr(sys.stdin)
+    tty.setcbreak(sys.stdin)
     board = Board()
     host = -1
     running = True
@@ -186,29 +212,42 @@ def main():
     winner = '#'
     report = ""
     while running:
-        os.system("clear")
-        board.render(host)
-        if flag == -1:
-            print("that's not a valid move:", report)
-        elif flag == 1:
-            print(winner, "WINS!")
-            running = False
+        action = 4
+        running = render(board, host, action, player, flag, winner, report)
+        if not running:
             continue
-        print(player, "to play")
-        if host >= 0:
-            print("host x:", (host % 3) + 1, "y: ", (host // 3) + 1)
-        else:
-            print("specify host")
-        action = input("\n\n>")
+        motion = ''
+        while motion != chr(10) and motion != ' ':
+            motion = sys.stdin.read(1)[0]
+            if motion == 'w' and action // 3 != 0:
+                action -= 3
+            elif motion == 'a' and action % 3 != 0:
+                action -= 1
+            elif motion == 's' and action // 3 != 2:
+                action += 3
+            elif motion == 'd' and action % 3 != 2:
+                action += 1
+            elif motion == '\x1b':
+                motion = sys.stdin.read(2)[1]
+                print(motion)
+                if motion == 'A' and action // 3 != 0:
+                    action -= 3
+                elif motion == 'D' and action % 3 != 0:
+                    action -= 1
+                elif motion == 'B' and action // 3 != 2:
+                    action += 3
+                elif motion == 'C' and action % 3 != 2:
+                    action += 1
+            running = render(board, host, action, player, flag, winner, report)
         try:
-            action = int(action)-1
+            action = int(action)
         except ValueError:
             flag = -1
             report = "input not an integer"
             continue
-        if action > 9:
+        if action > 8 or action < 0:
             flag = -1
-            report = "input greater than 9"
+            report = "input out of range (1-9)"
             continue
         if host == -1:
             if board.check_open(action):
@@ -225,7 +264,8 @@ def main():
             if board.check_open(action):
                 host = action
             else:
-                host = -1
+                host = -1 
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, original_terminal_settings)
 
 
 if __name__ == "__main__":
